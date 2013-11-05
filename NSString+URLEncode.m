15aea576946b1d17daa44fde2d5879f33bb41360
Randolph Lee <vadoff@gmail.com>
2013-11-05 15:41:57 -0800

    Update NSString+URLEncode.m

--- NSString+URLEncode.m
+++ NSString+URLEncode.m
@@ -1,21 +1,33 @@
-
 #import "NSString+URLEncode.h"
 
 @implementation NSString (URLEncode)
 
 - (NSString *)urlEncodedString {
-  NSMutableCharacterSet *characterSet = [[NSMutableCharacterSet alloc] init];
-
   NSRange numRange = NSMakeRange((NSUInteger)'0', 10);
   NSRange AZRange = NSMakeRange((NSUInteger)'A', 26);
   NSRange azRange = NSMakeRange((NSUInteger)'a', 26);
   
+  NSMutableCharacterSet *characterSet = [[NSMutableCharacterSet alloc] init];
   [characterSet addCharactersInRange:numRange];
   [characterSet addCharactersInRange:AZRange];
   [characterSet addCharactersInRange:azRange];
   [characterSet addCharactersInString:@"~!@$&*()-_=:;',./?"];
   
-  return [self stringByAddingPercentEncodingWithAllowedCharacters:characterSet];
+  NSMutableString *output = [NSMutableString string];
+  const unsigned char *source = (const unsigned char *)self.UTF8String;
+  
+  NSUInteger sourceLen = strlen((const char *)source);
+  for (NSUInteger i = 0; i < sourceLen; i++) {
+    const unsigned char thisChar = source[i];
+
+    if ([characterSet characterIsMember:thisChar]) {
+      [output appendFormat:@"%c", thisChar];
+    } else {
+      [output appendFormat:@"%%%02X", thisChar];
+    }
+  }
+
+  return output;
 }
 
 @end
