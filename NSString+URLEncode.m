a9b403279ebcd6571f751dc4effb669935558192
Randolph Lee <vadoff@gmail.com>
2013-11-05 15:01:41 -0800

    Create NSString+URLEncode.m

---
+++ NSString+URLEncode.m
@@ -0,0 +1,18 @@
+#import "NSString+URLEncode.h"
+
+@implementation NSString (URLEncode)
+
+- (NSString *)urlEncodedString {
+  NSMutableCharacterSet *characterSet = [[NSMutableCharacterSet alloc] init];
+
+  NSRange numRange = NSMakeRange((NSUInteger)'0', 10);
+  NSRange AZRange = NSMakeRange((NSUInteger)'A', 26);
+  NSRange azRange = NSMakeRange((NSUInteger)'a', 26);
+  
+  [characterSet addCharactersInRange:numRange];
+  [characterSet addCharactersInRange:AZRange];
+  [characterSet addCharactersInRange:azRange];
+  [characterSet addCharactersInString:@"~!@$&*()-_=:;',./?"];
+  
+  return [self stringByAddingPercentEncodingWithAllowedCharacters:characterSet];
+}
